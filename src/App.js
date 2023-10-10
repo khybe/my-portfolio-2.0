4564c0c33c1e08a730c9658ef3f0a6b8bab45884
Khaibar Saadat <khybersaadat93@gmail.com>
2023-10-10 22:45:52 +0200

    Initial project setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./components/home/Home";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <header>{/* <p> Here goes Navbar</p> */}</header>
+      <main>
+        <Home />
+        <section id="about-me">
+          <h2>About Me</h2>
+        </section>
+        <section id="my-work">
+          <h2>My Work</h2>
+        </section>
+        <section id="contact-me">
+          <h2>Contact Me</h2>
+        </section>
+      </main>
     </div>
   );
 }
