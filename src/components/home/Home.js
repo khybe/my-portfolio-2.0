8743a63581900cdf671379c8c4b273b9ad3a643e
Khaibar Saadat <khybersaadat93@gmail.com>
2023-10-11 22:46:17 +0200

    Update CSS file name from Home.module.css to Home.css and refactor styles to prevent fixed navbar overlap

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -5,20 +5,20 @@ import greetingImageLarge from "../../assets/greeting-1920x1080.gif";
 import hireMeGif from "../../assets/hire-me-500x500.gif";
 
 // Import CSS styles for the component
-import styles from "./Home.module.css";
+import "./Home.css";
 
 const Home = () => {
   // Destructure CSS class names from the imported styles object
-  const { hero, heroContent, tagline, mottoContainer, helloAnimeContainer } =
-    styles;
+  // const { hero, heroContent, tagline, mottoContainer, helloAnimeContainer } =
+  //   styles;
 
   return (
-    <section id={hero}>
-      <div className={heroContent}>
-        <p className={tagline}>
+    <section id="hero">
+      <div className="heroContent">
+        <p className="tagline">
           A <span>Front-end Developer.</span>
         </p>
-        <div className={mottoContainer}>
+        <div className="mottoContainer">
           <p>
             I believe in brevity and minimalism to create dynamic, sleek, and
             user-friendly web apps.
@@ -28,11 +28,11 @@ const Home = () => {
             <img src={hireMeGif} alt="Hire me animation in the form of a GIF" />
           </a>
         </div>
-        <div className={helloAnimeContainer}>
+        <div className="helloAnimeContainer">
           <picture>
             {/* Use responsive images based on viewport width */}
-            <source srcset={greetingImageSmall} media="(max-width: 768px)" />
-            <source srcset={greetingImageMedium} media="(max-width: 1152px)" />
+            <source srcSet={greetingImageSmall} media="(max-width: 768px)" />
+            <source srcSet={greetingImageMedium} media="(max-width: 1152px)" />
             <img
               src={greetingImageLarge}
               alt="Hello, I am Khaibar! animation"
