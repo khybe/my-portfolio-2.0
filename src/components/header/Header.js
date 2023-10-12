75d5d3eceea71c873376be4605928adb5c5babae
Khaibar Saadat <khybersaadat93@gmail.com>
2023-10-12 17:52:10 +0200

    Implement responsive backdrop and adjust logo size/position

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,21 +1,40 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
+import Backdrop from "../../UI/Backdrop";
 import logoImage from "../../assets/logo.gif";
 
 import styles from "./Header.module.css";
 
 function Header() {
   const [isOpen, setIsOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 769);
 
   const toggleNavbarHandler = () => {
-    setIsOpen(!isOpen);
+    if (isMobile) {
+      setIsOpen((prevState) => !prevState);
+    }
   };
+
+  useEffect(() => {
+    // Add an event listener to detect window width changes and update isMobile state
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 769);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   const { navbar, logo, menuButton, bar, navLinks, open } = styles;
 
   return (
     <header>
       <nav className={navbar}>
+        {isMobile && isOpen && <Backdrop onClick={toggleNavbarHandler} />}
         <div className={logo}>
           <a href="#hero">
             <img src={logoImage} alt="Khaibar Portfolio Logo" />
