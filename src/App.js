ae668fc7f64ffd1af6cdb136169098ddeb92df03
Khaibar Saadat <khybersaadat93@gmail.com>
2023-11-02 23:51:35 +0100

    Refactor: Implement lazy loading for components in App.js and deploy to Firebase

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React, { lazy, Suspense } from "react";
+
 import Header from "./components/header/Header"; // Import the Header component
 import Home from "./components/home/Home"; // Import the Home component
 import AboutMe from "./components/about/AboutMe"; // Import the AboutMe component
-import MyWork from "./components/my-work/MyWork"; // Import the MyWork component
-import ContactMe from "./components/contact/ContactMe"; // Import the ContactMe component
-import Footer from "./components/footer/Footer"; // Import the Footer component
+
+const MyWork = lazy(() => import("./components/my-work/MyWork"));
+const ContactMe = lazy(() => import("./components/contact/ContactMe"));
+const Footer = lazy(() => import("./components/footer/Footer"));
 
 function App() {
   return (
@@ -12,10 +15,19 @@ function App() {
       <main>
         <Home /> {/* Render the Home component */}
         <AboutMe /> {/* Render the AboutMe component */}
-        <MyWork /> {/* Render the MyWork component */}
-        <ContactMe /> {/* Render the ContactMe component */}
+        <Suspense fallback={<p>Loading...</p>}>
+          <MyWork />
+        </Suspense>{" "}
+        {/* Render the MyWork component */}
+        <Suspense fallback={<p>Loading...</p>}>
+          <ContactMe />
+        </Suspense>{" "}
+        {/* Render the ContactMe component */}
       </main>
-      <Footer /> {/* Render the Footer component */}
+      <Suspense fallback={<p>Loading...</p>}>
+        <Footer />{" "}
+      </Suspense>{" "}
+      {/* Render the Footer component */}
     </div>
   );
 }
