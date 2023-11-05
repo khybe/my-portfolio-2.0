4af8fbd5855e0398e9eeff30a233bee7e2602f2b
Khaibar Saadat <khybersaadat93@gmail.com>
2023-11-05 19:35:43 +0100

    Enhance: Revise project display for Todo App and web-linked projects

--- src/components/my-work/components/Project.js
+++ src/components/my-work/components/Project.js
@@ -1,28 +1,32 @@
 import React from "react"; // Import React library
 import styles from "./Project.module.css"; // Import styles from the Project module
 
-const Project = ({ project, onOpenModal }) => (
-  <div className={styles["project-item"]}>
-    {" "}
-    {/* Display project item using defined styles */}
-    <h3>{project.title}</h3> {/* Render the project title */}
+const Project = ({ project, onOpenModal }) => {
+  const renderImage = project.projectLink ? ( // Check if project.projectLink exists
     <a href={project.projectLink} target="_blank" rel="noreferrer">
-      {" "}
-      {/* Link to the project, open in a new tab */}
-      <img src={project.image} alt={project.title} />{" "}
-      {/* Display the project image */}
+      <img src={project.image} alt={project.title} />
     </a>
-    <button
-      className={
-        styles["details-button"]
-      } /* Apply styles to the details button */
-      onClick={() =>
-        onOpenModal(project)
-      } /* Trigger the function to open the modal on button click */
-    >
-      Project Details {/* Button text for project details */}
-    </button>
-  </div>
-);
+  ) : (
+    <img src={project.image} alt={project.title} />
+  );
+
+  return (
+    <div className={styles["project-item"]}>
+      {/* Display project item using defined styles */}
+      <h3>{project.title}</h3> {/* Render the project title */}
+      {renderImage} {/* Render the image based on the condition */}
+      <button
+        className={
+          styles["details-button"]
+        } /* Apply styles to the details button */
+        onClick={() =>
+          onOpenModal(project)
+        } /* Trigger the function to open the modal on button click */
+      >
+        Project Details {/* Button text for project details */}
+      </button>
+    </div>
+  );
+};
 
 export default Project; // Export the Project component as the default export
