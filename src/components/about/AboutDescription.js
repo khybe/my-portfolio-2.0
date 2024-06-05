c1deda7b04b2b1155bbc7a80424ba784a08f9d6a
Khaibar Saadat <khybersaadat93@gmail.com>
2024-06-05 17:45:24 +0200

    Update AboutDescription components text and adjust it's styling

--- src/components/about/AboutDescription.js
+++ src/components/about/AboutDescription.js
@@ -9,35 +9,30 @@ const AboutDescription = () => {
 
   return (
     <article className={aboutDescription}>
-      {" "}
       {/* Assigning the aboutDescription class */}
       <Fade left>
-        {" "}
         {/* Fade animation effect from the left */}
-        <h2 className={aboutTitle}>ABOUT ME</h2>{" "}
+        <h2 className={aboutTitle}>ABOUT ME</h2>
         {/* Applying the aboutTitle class */}
       </Fade>
       <Fade right>
-        {" "}
         {/* Fade animation effect from the right */}
         <p>
           {/* Description of the individual, showcasing their skills and experiences */}
-          I am Khaibar Saadat, a self-taught front-end developer with a strong
-          passion for technology and a talent for creating dynamic websites.
+          Hello, I'm Khaibar Saadat, a self-taught full-stack developer with a
+          passion for crafting dynamic applications. As a student since 2019 and
+          an independent freelancer since November 2023, I've honed my skills.
+          Among most, I specialize in frontend and backend development,
+          particularly in the MERN stack, excelling in crafting responsive
+          websites, robust APIs, and integrated authentication and automatic
+          email systems. My focus extends to ensuring seamless user experiences
+          and efficient CRUD operations.
           <br />
           <br />
-          Over the past four years, I've delved deep into a variety of
-          programming languages and tools, continuously refining my skills. My
-          journey has been an ongoing exploration of learning, with a relentless
-          drive to achieve more. In addition to front-end expertise, I am also
-          familiar with MERN stack development, making me well-prepared for
-          full-stack challenges.
-          <br />
-          <br />
-          Whether you have a project in mind, seek collaboration, or wish to
-          explore my portfolio, I invite you to visit my workspace or reach out
-          to me directly. Your web development aspirations are just a message
-          away from becoming a reality.
+          Whether you have a specific project in mind or are seeking
+          collaboration, I invite you to explore my portfolio and reach out to
+          me. Your web development aspirations are just a message away from
+          becoming a reality.
         </p>
         <MySkills /> {/* Rendering the MySkills component */}
       </Fade>
@@ -45,4 +40,4 @@ const AboutDescription = () => {
   );
 };
 
-export default AboutDescription; // Exporting the AboutDescription component
+export default AboutDescription;
