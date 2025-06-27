654a7f16bbc77271bceaa92796e7bdf6b8cd063f
Khaibar Saadat <khybersaadat93@gmail.com>
2025-06-27 16:57:27 +0200

    Fix: update Navlinks layout and add missing react-reveal module declaration

--- react-reveal.d.ts
+++ react-reveal.d.ts
@@ -2,3 +2,4 @@ declare module "react-reveal/Fade";
 declare module "react-reveal/Slide";
 declare module "react-reveal/Zoom";
 declare module "react-reveal/LightSpeed";
+declare module "react-reveal";
